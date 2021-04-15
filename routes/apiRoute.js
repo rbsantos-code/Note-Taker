1323454d58b2579a3b368c03ebd4cde0bfb48e36
rbsantos-code <richardbriansantos@richards-mbp.attlocal.net>
2021-04-14 22:32:36 -0700

    create functions and fix id

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,23 +1,33 @@
 // imports
 const router = require('express').Router();
-const fs = require('fs');
-
+const saveNote = require('../db/saveNote');
 
 
 // notes get route
-router.get('/api/notes', (req, res) => {
-  
+router.get('/notes', (req, res) => {
+  saveNote
+  .getNotesData()
+  .then(notes => res.json(notes))
+  .catch(err => res.status(500).json(err));
 });
 
 
 // notes post route
-router.post('/api/notes', (req, res) => {
-    
+router.post('/notes', (req, res) => {
+    saveNote
+    .add(req.body)
+    .then((notes) => res.json(notes))
+    .catch(err => res.status(500).json(err));
 });
 
 // notes get specific id
-router.get('/api/notes/:id', (req, res) => {
- 
+router.get('/notes/:id', (req, res) => {
+   
+});
+
+// note delete route
+router.delete('/notes/:id', (req, res) => {
+    
 });
 
 module.exports = router;
