8f41fd103640defc8bb80493d76465ad76337ae6
rbsantos-code <richardbriansantos@richards-mbp.attlocal.net>
2021-04-14 19:02:50 -0700

    apiRoute inProgress

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,2 +1,23 @@
 // imports
 const router = require('express').Router();
+const fs = require('fs');
+
+
+
+// notes get route
+router.get('/api/notes', (req, res) => {
+  
+});
+
+
+// notes post route
+router.post('/api/notes', (req, res) => {
+    
+});
+
+// notes get specific id
+router.get('/api/notes/:id', (req, res) => {
+ 
+});
+
+module.exports = router;
